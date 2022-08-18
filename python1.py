ed22066d2bc2f7e118e2c0011e9e2076b2880ed7
Sadia Zamal <61104968+sadiazamal123@users.noreply.github.com>
2022-08-19 01:34:08 +0600

    .

---
+++ python1.py
@@ -0,0 +1,16 @@
+# Python3 program to add two numbers (take input from user)
+
+# num1 = input("Type a number: ")
+# num2 = input("Type another number: ")
+# sum = int(num1) + int(num2)
+#print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# Python3 program to add two numbers (value is user defined)
+num1 = 15
+num2 = 12
+sum = float(num1) + float(num2)
+print("The sum is: ", sum)
+
+
+ 
+
