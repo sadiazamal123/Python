334af08f1bc9b739e3f405176d1b38328fc0a2d0
Sadia Zamal <61104968+sadiazamal123@users.noreply.github.com>
2022-08-19 01:35:59 +0600

    .

---
+++ python7.py
@@ -0,0 +1,10 @@
+# python code to print a string using f-strings
+grade = "A"
+marks = 90
+print(f"John doe obtained {grade} grade with {marks} marks.")
+
+# python code to print a string using concatenation
+# grade = "A"
+# marks = 90
+# print("John doe obtained " + grade + " grade with " + str(marks) + " marks.")
+
